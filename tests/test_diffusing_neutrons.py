932c852039883afcf7ff50cb13165a323912e97e
DerkNiessink <derk@niessink.com>
2024-02-05 16:44:38 +0100

    Added tests for DiffusingNeutrons methods

--- tests/test_diffusing_neutrons.py
+++ tests/test_diffusing_neutrons.py
@@ -3,139 +3,102 @@ import numpy as np
 
 from neutrons.diffusing_neutrons import DiffusingNeutrons
 
-
-@pytest.fixture
-def get_diffusing_neutron():
-    return DiffusingNeutrons(
-        [1, 2, 3],
-        [1, 2, 3],
-        [np.array([0, 0, 0])],
-        [1],
-        1,
-        1,
-        np.array([0, 0, 0]),
-        0.920,
-    )
-
-
-@pytest.fixture
-def get_diffusing_neutron_2():
-    return DiffusingNeutrons(
-        [5, 3, 3],
-        [1, 2, 8],
-        [np.array([1, 2, 2])],
-        [20],
-        5,
-        3,
-        np.array([0, 5, 0]),
-        0.2,
-    )
+test_data = [
+    {
+        "class_object": DiffusingNeutrons(
+            [5.0, 3.0, 3.0],
+            [1.0, 2.0, 8.0],
+            [np.array([0, 0, 0]), np.array([1, 0, 0]), np.array([0, 1, 0])],
+            [2 * 10**6, 2 * 10**6, 2 * 10**6],
+            50,
+            50,
+            np.array([0, 0, 0]),
+            0.920,
+        ),
+        "energy_expected_dict": {6.0: 3, 2.0: 3, 0.1: 5.0},
+        "expected_collisions": 16,
+    },
+    {
+        "class_object": DiffusingNeutrons(
+            [1.0, 2.0, 3.0],
+            [1.0, 2.0, 3.0],
+            [np.array([0, 0, 0])],
+            [2 * 10**6],
+            50,
+            50,
+            np.array([0, 0, 0]),
+            0.725,
+        ),
+        "energy_expected_dict": {2.0: 2.0, 1.6: 2.0, 8.0: 3.0},
+        "expected_collisions": 20,
+    },
+]
 
 
 class TestDiffusingNeutrons:
-    @pytest.mark.parametrize(
-        "mean_free_paths, energies, initial_positions, initial_energies, radius_tank, height_tank, position_tank, xi",
-        [
-            (
-                [1, 2, 3],
-                [1, 2, 3],
-                [np.array([0, 0, 0])],
-                [1],
-                1,
-                1,
-                np.array([0, 0, 0]),
-                0.920,
-            ),
-            (
-                [1, 2, 3],
-                [1, 2, 3],
-                [np.array([0, 0, 0])],
-                [1],
-                1,
-                1,
-                np.array([0, 0, 0]),
-                0.920,
-            ),
-            (
-                [1, 2, 3],
-                [1, 2, 3],
-                [np.array([0, 0, 0])],
-                [1],
-                1,
-                1,
-                np.array([0, 0, 0]),
-                0.920,
-            ),
-        ],
-    )
-    def test_init(
-        self,
-        mean_free_paths,
-        energies,
-        initial_positions,
-        initial_energies,
-        radius_tank,
-        height_tank,
-        position_tank,
-        xi,
-    ):
-        diffusing_neutrons = DiffusingNeutrons(
-            mean_free_paths,
-            energies,
-            initial_positions,
-            initial_energies,
-            radius_tank,
-            height_tank,
-            position_tank,
-            xi,
-        )
-        assert diffusing_neutrons.energy_loss_frac == 1 / np.exp(xi)
-        assert diffusing_neutrons.mean_free_paths == mean_free_paths
-        assert diffusing_neutrons.energies == energies
-        assert diffusing_neutrons.nNeutrons == len(initial_positions)
-        assert diffusing_neutrons.tank.radius == radius_tank
-        assert diffusing_neutrons.tank.height == height_tank
-        assert np.all(diffusing_neutrons.tank.position == position_tank)
-        assert diffusing_neutrons.tank.xi == xi
-        assert diffusing_neutrons.tank.volume == np.pi * radius_tank**2 * height_tank
-        assert diffusing_neutrons.tank.energy_loss_frac == 1 / np.exp(xi)
-        assert diffusing_neutrons.neutrons.energies == initial_energies
-        assert np.all(diffusing_neutrons.neutrons.positions == initial_positions)
 
-    def test_random_directions(self):
-        diffusing_neutrons = DiffusingNeutrons(
-            [1, 2, 3],
-            [1, 2, 3],
-            [np.array([0, 0, 0])],
-            [1],
-            1,
-            1,
-            np.array([0, 0, 0]),
-            0.920,
-        )
+    # Only use the DiffusingNeutrons objects
+    @pytest.mark.parametrize(
+        "diffusing_neutrons",
+        [test_data[0]["class_object"], test_data[1]["class_object"]],
+    )
+    def test_random_directions(self, diffusing_neutrons: DiffusingNeutrons) -> None:
+        """
+        Test the random directions of the neutrons, which should have a norm of 1.
+        """
         N = 10
         directions = diffusing_neutrons._random_directions(N)
         assert len(directions) == N
         for direction in directions:
             assert np.linalg.norm(direction) == pytest.approx(1, abs=1e-6)
 
+    # Use the DiffusingNeutrons objects and the expected energy values.
     @pytest.mark.parametrize(
-        "energy, expected",
+        "diffusing_neutrons, energy_expected_dict",
         [
-            (5, 2),
-            (4, 2),
-            (2, 3),
+            (test_data[0]["class_object"], test_data[0]["energy_expected_dict"]),
+            (test_data[1]["class_object"], test_data[1]["energy_expected_dict"]),
         ],
     )
-    def test_mf_lookup(self, energy, expected):
-        diffusing_neutrons = DiffusingNeutrons(
-            [1, 2, 3],
-            [3, 4, 2],
-            [np.array([0, 0, 0])],
+    def test_mf_lookup(
+        self, diffusing_neutrons: DiffusingNeutrons, energy_expected_dict: dict
+    ) -> None:
+        for energy, expected in energy_expected_dict.items():
+            assert diffusing_neutrons._mf_lookup(energy) == expected
+
+    # Use only the first DiffusingNeutrons object.
+    @pytest.mark.parametrize(
+        "diffusing_neutrons, expected_collisions",
+        [
+            (test_data[0]["class_object"], test_data[0]["expected_collisions"]),
+            (test_data[1]["class_object"], test_data[1]["expected_collisions"]),
+        ],
+    )
+    def test_diffuse(
+        self, diffusing_neutrons: DiffusingNeutrons, expected_collisions: int
+    ) -> None:
+        """
+        Test the diffuse method and the final energy of the neutrons, which
+        should be 1 eV for both tests (H20 and D as moderators).
+        """
+        diffusing_neutrons.diffuse(nCollisions=expected_collisions - 5)
+        diffusing_neutrons.diffuse(nCollisions=5)
+        assert diffusing_neutrons.get_energies()[0][-1] == pytest.approx(1, abs=0.2)
+
+    def test_get_number_escaped(self) -> None:
+        """
+        Test the get_number_escaped method, in the case all neutrons should escape
+        from the tank.
+        """
+        neutrons = DiffusingNeutrons(
             [1],
+            [2],
+            [np.array([0.1, 0, 0]), np.array([1.01, 0, 0]), np.array([0.5, 0, 0])],
+            [2 * 10**6, 2 * 10**6, 2 * 10**6],
             1,
             1,
             np.array([0, 0, 0]),
             0.920,
         )
-        assert diffusing_neutrons._mf_lookup(energy) == expected
+        neutrons.diffuse(nCollisions=30)
+        assert neutrons.get_number_escaped() == 3
